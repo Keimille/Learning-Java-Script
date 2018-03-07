601caeb372cb9b6cecc18c4bd46ea6180c3577d9
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-06 21:45:06 -0500

    Update Control Flow.js

--- Control Flow.js
+++ Control Flow.js
@@ -1,9 +1,24 @@
 //Control Flow
 
+//the following if/else statemnt would print 'Why do you like soccer?'
 
 let isSoccerfan = true;
 if (isSoccerfan = true) {
   console.log('Why do you like soccer?');
 } else {
   console.log('What kind of person does not like soccer?');
+}
+
+//the added if/else prints 'Why do you like soccer?' 'Goal!'
+
+let isSoccerFan = true;
+if (isSoccerFan = true) {
+  console.log('Why do you like soccer?');
+} else {
+  console.log('What kind of person does not like soccer?');
+}
+if (isSoccerFan = true){
+  console.log('Goal!');
+} else {
+  console.log('No goal!');
 }
