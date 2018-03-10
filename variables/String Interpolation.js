949c2f4e9f0cd79016c3a0adf6d6e5e5c0a7251b
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-09 22:08:06 -0500

    Update String Interpolation.js

--- variables/String Interpolation.js
+++ variables/String Interpolation.js
@@ -5,3 +5,9 @@
 let favoriteAnimal = 'Liger';
 console.log('My favorite animal:' + favoriteAnimal);
 //ouput: My favorite animal:Liger
+
+//use bactick '`' to wrap entire string and ${} to enter variables
+let myName = 'Keith';
+let myCity = 'Busytown';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`)
+//output: My name is Keith. My favorite city is Busytown
