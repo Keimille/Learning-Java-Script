e1be602645a217c45920430b14eb59070feebda4
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-09 21:50:20 -0500

    Create mathematical assignment operators.js

---
+++ variables/mathematical assignment operators.js
@@ -0,0 +1,16 @@
+//mathematical Assignment operators
+
+let x = 4;
+x += 2; // x equals 6
+
+let y = 4;
+y -= 2; // y equals 2
+
+let z = 4;
+z *= 2; // z equals 8
+
+let r = 4;
+r++; // r equals 5
+
+let t = 4;
+t--; // t equals 3
