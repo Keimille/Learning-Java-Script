20c91378e5750aecd99e096500e5e89ab2b139bb
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-09 21:32:07 -0500

    Create let & const.js

---
+++ variables/let & const.js
@@ -0,0 +1,27 @@
+//Examples of variables
+
+//Variables allow us to assign data to a word and use the word to reference the data
+
+//declare variables withl let const. const is short for constant
+
+const entree = 'Enchiladas';
+const price = 12;
+console.log(entree);
+console.log(price);
+//output: Enchiladas
+//output: 12
+
+//you can not assign to a constant a different variable
+//see example
+const entree = 'Enchiladas';
+const price = 12;
+console.log(entree);
+console.log(price);
+entree = 'Tacos';
+//output: TypeError: Assignment to constant variable
+
+//let variables can be reassigned 
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+//output: false
