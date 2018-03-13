a94dc24da75197ddf315a7f6fb463694f01a5c60
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-12 21:16:29 -0400

    Create comparison operators.js

---
+++ control flow/comparison operators.js
@@ -0,0 +1,13 @@
+//Comparison Operators
+
+//to check if two things are equal to each other, write ===
+//to check if two things do not equal each other, write !==
+
+let moonPhase = 'full';
+
+if (moonPhase === 'full') {
+  console.log('Howl!');
+} else {
+  console.log('I swear I am not a werewolf.');
+}
+//output: Howl!
