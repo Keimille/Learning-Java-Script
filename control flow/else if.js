d47ede37b2ce1b6ab4720cbd1b77828f91f8e794
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-13 22:03:37 -0400

    Update and rename if_esle.js to else if.js

--- control flow/else if.js
+++ control flow/else if.js
@@ -22,3 +22,17 @@ if (isSoccerFan = true){
 } else {
   console.log('No goal!');
 }
+
+//else if statments and operators
+let moonPhase = 'mostly full';
+
+if (moonPhase === 'full') {
+  console.log('Howl!');
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier');
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
+} else {
+  console.log('Invalid moon phase');
+}
+//output: Arms and legs are getting harier
