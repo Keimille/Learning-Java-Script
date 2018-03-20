b0d762ccdc22ba783394e4c7aacf214de0657a11
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-19 23:19:29 -0400

    Update and rename Introduction to Introduction.js

---
+++ Functions/Introduction.js
@@ -0,0 +1,10 @@
+//A function is a block of code designed to perform a task.
+// An arrow token '()=>' indicates that variable stores a function
+//Between the curly braces '{}' is the function body
+//Call a function by writing its name folowed by a simicolon ex. myFunction();
+
+const takeOrder = ()=> {
+  console.log('Order:pizza')
+}
+takeOrder();
+//output: Order:pizza
