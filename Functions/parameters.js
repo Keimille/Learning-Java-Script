218b8a80e5f193bf556a0f4996483157908f9136
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-21 21:03:32 -0400

    Update parameters.js

--- Functions/parameters.js
+++ Functions/parameters.js
@@ -5,3 +5,13 @@ const takeOrder = (topping) => {
 
 takeOrder('mushrooms');
 //output: Order: pizza topped with mushrooms
+
+//example of multiple parameters
+//seperate paramaters with a comma
+
+const takeOrder = (crustType, topping) => {
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('thin crust','bacon');
+//output: Order: thin crust pizza topped with bacon
