aea7d22e5b9624684bd0609782f2e7ea66cd7f59
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-21 20:47:14 -0400

    Create parameters.js

---
+++ Functions/parameters.js
@@ -0,0 +1,7 @@
+//example of parameters
+const takeOrder = (topping) => {
+  console.log('Order: pizza topped with ' + topping);
+};
+
+takeOrder('mushrooms');
+//output: Order: pizza topped with mushrooms
