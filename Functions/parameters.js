af2f0d465b1e239e235b00d3e019585af03bd01b
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-03-29 06:53:10 -0400

    Update parameters.js

--- Functions/parameters.js
+++ Functions/parameters.js
@@ -15,3 +15,17 @@ const takeOrder = (crustType, topping) => {
 
 takeOrder('thin crust','bacon');
 //output: Order: thin crust pizza topped with bacon
+
+// this next code took some time to wrap my head around. worth reviewing 
+
+const orderCount = (0);
+const takeOrder = (crustType, topping, orderCount) => {
+orderCount = orderCount + 1;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+const getSubTotal = (itemCount) => {
+ return (itemCount) * 7.5; 
+};
+console.log(getSubTotal(orderCount))
+//output: 0
